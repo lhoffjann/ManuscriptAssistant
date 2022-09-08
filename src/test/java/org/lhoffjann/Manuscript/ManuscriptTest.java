3829d577fccad137245e0e5558e259e9d71baea2
lhoffjann <lennart.hoffjann@uni-bielefeld.de>
2022-09-08 09:53:49 +0200

    deleted comment

--- src/test/java/org/lhoffjann/Manuscript/ManuscriptTest.java
+++ src/test/java/org/lhoffjann/Manuscript/ManuscriptTest.java
@@ -26,31 +26,6 @@ public class ManuscriptTest {
         actualManuscript = testManuscriptFactory.createManuscript("MS_2877_test");
     }
 
-        /**
-        FaksimileFactory faksimileFactory = new FaksimileFactory();
-        testManuscript = new Manuscript("MS_2877_test");
-        List<Page> testPages = Arrays.asList(
-                new Page(1, faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0001_V.tif"), 1), faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0002_RL.tif"), 2)),
-                new Page(2, faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0003_V.tif"), 3), faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0004_RL.tif"), 4)),
-                new Page(3, faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0005_V.tif"), 5), faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0006_RL.tif"), 6)),
-                new Page(4, faksimileFactory.createFaksimile(Paths.get("/mnt/larchiv/manuskripte_scans_fuer_digitalisierung/scans/MS_2877_test/MS_2877_0007_F.tif"), 7), faksimileFactory.createFaksimile(null, 8)));
-        testManuscript.setPages(testPages);
-    }
-
-        @Test
-        public void createManuscript () {
-            System.out.println("this is me");
-            for (int i = 0; i < testManuscript.getPageList().size(); i++) {
-                assertEquals(testManuscript.getPageList().get(i).getId(), actualManuscript.getPageList().get(i).getId());
-                assertEquals(testManuscript.getPageList().get(i).getFront().getOrderNumber(), actualManuscript.getPageList().get(i).getFront().getOrderNumber());
-                assertEquals(testManuscript.getPageList().get(i).getFront().getUniqueID(), actualManuscript.getPageList().get(i).getFront().getUniqueID());
-                assertEquals(testManuscript.getPageList().get(i).getFront().getPath(), actualManuscript.getPageList().get(i).getFront().getPath());
-                assertEquals(testManuscript.getPageList().get(i).getBack().getOrderNumber(), actualManuscript.getPageList().get(i).getBack().getOrderNumber());
-                assertEquals(testManuscript.getPageList().get(i).getBack().getUniqueID(), actualManuscript.getPageList().get(i).getBack().getUniqueID());
-                assertEquals(testManuscript.getPageList().get(i).getBack().getPath(), actualManuscript.getPageList().get(i).getBack().getPath());
-            }
-        }
-**/
     @Test
     public void checkPageTypeIdentifier () {
     for( Page page: actualManuscript.getPageList()){
